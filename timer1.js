ada5b7f95f4165bb12e1e87bcdf33a57d18f314e
Alexys Groom <lexygroom@gmail.com>
2023-08-09 21:08:45 -0400

    Implement timer1

---
+++ timer1.js
@@ -0,0 +1,17 @@
+//beep after a specified amount of time
+//Example usage: node timer1.js 10 3 5 15 9 
+
+const args = process.argv.slice(2);
+
+const timer = function(times) {
+  for (const time of times) {
+    const seconds = Number(time);
+    if (seconds > 0) {
+      setTimeout(() => {
+        process.stdout.write('\x07');
+        }, seconds * 1000) 
+    }
+  }
+};
+
+timer(args);
